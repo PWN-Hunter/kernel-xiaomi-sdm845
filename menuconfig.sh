5454a372663a1d70006d2fdebe7fe6b2d50a0a05
Martin Valba (PC) <kailing2@live.com>
2020-08-12 17:28:19 +0300

    Did some modifications

--- menuconfig.sh
+++ menuconfig.sh
@@ -1,10 +1,13 @@
+# This script will make temp out dir in ../out
+
 export ARCH=arm64
 
 export SUBARCH=arm64
 
-make O=../out skull_defconfig
-make O=../out menuconfig
-cp -rf ../out/.config arch/arm64/configs/skull_defconfig
+make O=../out_menu skull_defconfig
+make O=../out_menu menuconfig
+cp -rf ../out_menu/.config arch/arm64/configs/skull_defconfig
+rm -rf ../out_menu
 echo " "
 echo " "
 echo " "
