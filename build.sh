0dbd8b269930c04378e3bfdb70e274f3e860a8f3
Martin Valba (PC) <kailing2@live.com>
2020-08-24 15:18:33 +0300

    Add toolchains and build.sh for people that dont use aosp env

--- build.sh
+++ build.sh
@@ -1,70 +1,20 @@
-#!/bin/bash
+# Select this dir for ROMDIR
 
-#set -e
+export ROMDIR=$(pwd)
 
-## Copy this script inside the kernel directory
-KERNEL_DEFCONFIG=beryllium_defconfig
-ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=Optimus_Drunk_Beryllium_v10.27.zip
-export PATH="$KERNELDIR/prebuilts/proton-clang/bin:${PATH}"
+# Select arch
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="$($KERNELDIR/prebuilts/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-# Speed up build process
-MAKE="./makeparallel"
 
-BUILD_START=$(date +"%s")
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
+# Use GCC toolchains from this dir
+KERNEL_TOOLCHAIN=$ROMDIR/toolchain/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+ARM32_TOOLCHAIN=$ROMDIR/toolchain/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+# Clang is custom built for kernel 4.4.x
+CLANG_TOOLCHAIN=$ROMDIR/toolchain/clang/bin/clang-10
 
-# Clean build always lol
-echo "**** Cleaning ****"
-mkdir -p out
-make O=out clean
+export CROSS_COMPILE=$KERNEL_TOOLCHAIN
+export CROSS_COMPILE_ARM32=$ARM32_TOOLCHAIN
 
-echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
-echo -e "$blue***********************************************"
-echo "          BUILDING KERNEL          "
-echo -e "***********************************************$nocol"
-make $KERNEL_DEFCONFIG O=out
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-                      NM=llvm-nm \
-                      OBJCOPY=llvm-objcopy \
-                      OBJDUMP=llvm-objdump \
-                      STRIP=llvm-strip
-
-echo "**** Verify Image.gz-dtb ****"
-ls $PWD/out/arch/arm64/boot/Image.gz-dtb
-
-#Anykernel 2 time!!
-echo "**** Verifying AnyKernel3 Directory ****"
-ls $ANYKERNEL3_DIR
-echo "**** Removing leftovers ****"
-rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
-rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
-
-echo "**** Copying Image.gz-dtb ****"
-cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
-
-echo "**** Time to zip up! ****"
-cd $ANYKERNEL3_DIR/
-zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $KERNELDIR/$FINAL_KERNEL_ZIP
-
-echo "**** Done, here is your sha1 ****"
-cd ..
-rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
-rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
-rm -rf out/
-
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
-sha1sum $KERNELDIR/$FINAL_KERNEL_ZIP
+echo "Starting compile"
+make O=../out skull_defconfig
+make O=../out CC=$CLANG_TOOLCHAIN CLANG_TRIPLE=aarch64-linux-gnu
